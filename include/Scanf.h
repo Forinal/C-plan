7033400e8b900bcf0bdaa900239987314a9464b9
Forinal <1715354220@qq.com>
2018-01-29 17:47:57 +0800

    Add files via upload

---
+++ include/Scanf.h
@@ -0,0 +1,10 @@
+#ifndef SCANF_H
+#define SCANF_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h> //Ã·π©∫Íva_list,va_start,va_arg,va_end
+#include <ctype.h>
+
+int Scanf(FILE *fp,char *format,...);
+#endif // SCANF_C
